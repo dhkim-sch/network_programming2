aae20ddfc1584fc01427fecd08f958c05de193ca
Daehee Kim <dhkim.sch@gmail.com>
2021-03-21 07:55:27 +0900

    Delete q

--- echo_client.py
+++ echo_client.py
@@ -1,15 +1,13 @@
 import socket
 port = int(input("Port No:"))
-address = ("localhost", port)
+address = ("localhost", port)
 BUFSIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(address)
 while True:
     msg = input("Message to send: ")
-    if msg == 'q':
-        break
     s.send(msg.encode())
-    data = s.recv(BUFSIZE)
-    print("Received message: %s" %data.decode())
+    data = s.recv(BUFSIZE)
+    print("Received message: %s"%data.decode())
 
 s.close()
