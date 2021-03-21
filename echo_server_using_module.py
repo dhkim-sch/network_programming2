bd4ed1bc38bbfd895697b66d0f88f3312ced0995
Daehee Kim <dhkim.sch@gmail.com>
2021-03-21 09:45:07 +0900

    Echo server update 1

--- echo_server_using_module.py
+++ echo_server_using_module.py
@@ -6,9 +6,10 @@ sock = my.TCPServer(port)
 while True:
     conn, addr = sock.Accept()
     print('connected by', addr)
-    data = conn.recv(BUFSIZE)
-    if not data:
-        break
-    print("Received message: ", data.decode())
-    conn.send(data)
-conn.close()
+    while True:
+        data = conn.recv(BUFSIZE)
+        if not data:
+            break
+        print("Received message: ", data.decode())
+        conn.send(data)
+    conn.close()
